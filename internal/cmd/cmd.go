450c04cbc391c207652a4df01989b6c8c26faecf
Wisansiiz <18257719723@163.com>
2024-02-29 22:23:54 +0800

    feat: 跨域

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -2,11 +2,10 @@ package cmd
 
 import (
 	"context"
-	"voichatter/internal/controller/user"
-
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
+	"voichatter/internal/controller/user"
 
 	"voichatter/internal/controller/hello"
 )
@@ -45,11 +44,15 @@ var (
 		Brief: "start http server",
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
+			s.Use(ghttp.MiddlewareHandlerResponse)
 			s.Group("/", func(group *ghttp.RouterGroup) {
+				// Group middlewares.
+				group.Middleware(
+					ghttp.MiddlewareCORS,
+				)
 				group.GET("/swagger", func(r *ghttp.Request) {
 					r.Response.Write(swaggerUIPageContent)
 				})
-				group.Middleware(ghttp.MiddlewareHandlerResponse)
 				group.Bind(
 					hello.NewV1(),
 					user.NewV1(),
---
+++ internal/logic/middleware/middleware.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"voichatter/internal/service"
+
+	"github.com/gogf/gf/v2/net/ghttp"
+)
+
+type (
+	sMiddleware struct{}
+)
+
+func init() {
+	service.RegisterMiddleware(New())
+}
+
+func New() service.IMiddleware {
+	return &sMiddleware{}
+}
+
+// CORS allows Cross-origin resource sharing.
+func (s *sMiddleware) CORS(r *ghttp.Request) {
+	r.Response.CORSDefault()
+	r.Middleware.Next()
+}
