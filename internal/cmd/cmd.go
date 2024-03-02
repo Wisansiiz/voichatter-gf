9394a1a6818a244e095ff759180dfbfa64c67766
Wisansiiz <18257719723@163.com>
2024-03-02 21:09:46 +0800

    feat: 路由和中间件

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -5,9 +5,10 @@ import (
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
-	"voichatter/internal/controller/user"
-
+	"voichatter/internal/controller/chat"
 	"voichatter/internal/controller/hello"
+	"voichatter/internal/controller/user"
+	"voichatter/internal/service"
 )
 
 const (
@@ -44,20 +45,34 @@ var (
 		Brief: "start http server",
 		Func: func(ctx context.Context, parser *gcmd.Parser) (err error) {
 			s := g.Server()
-			s.Use(ghttp.MiddlewareHandlerResponse)
 			s.Group("/", func(group *ghttp.RouterGroup) {
-				// Group middlewares.
-				group.Middleware(
-					ghttp.MiddlewareCORS,
-				)
 				group.GET("/swagger", func(r *ghttp.Request) {
 					r.Response.Write(swaggerUIPageContent)
 				})
-				group.Bind(
-					hello.NewV1(),
-					user.NewV1(),
+				group.Middleware(
+					ghttp.MiddlewareCORS, //跨域中间件
+					service.Middleware().Ctx,
+					ghttp.MiddlewareHandlerResponse,
 				)
+				group.Group("/", func(group *ghttp.RouterGroup) {
+					group.POST("/", user.NewV1().SignIn)
+					group.POST("/", user.NewV1().SignUp)
+					//group.GET("/", user.NewV1().ServerList)
+				})
+				group.GET("/api/yy", func(r *ghttp.Request) {
+					chat.HandleWebSocket(r)
+				})
+				group.Group("/", func(group *ghttp.RouterGroup) {
+					group.Middleware(service.Middleware().Auth)
+					group.Bind(
+						hello.NewV1(),
+						user.NewV1().SignOut,
+						user.NewV1().ServerList,
+					)
+				})
 			})
+			s.EnableHTTPS("./cert/server.pem", "./cert/server-key.pem")
+			s.SetPort(9000)
 			s.Run()
 			return nil
 		},
