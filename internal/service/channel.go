fdafbf142edd2872637f3ebeb6338664ad36162a
Wisansiiz <18257719723@163.com>
2024-03-03 22:01:07 +0800

    feat: 创建频道

---
+++ internal/service/channel.go
@@ -0,0 +1,33 @@
+// ================================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// You can delete these comments if you wish manually maintain this interface file.
+// ================================================================================
+
+package service
+
+import (
+	"context"
+	v1 "voichatter/api/channel/v1"
+	"voichatter/internal/model"
+)
+
+type (
+	IChannel interface {
+		ChannelCreate(ctx context.Context, in model.ChannelCreateInput) (res *v1.ChannelCreateRes, err error)
+	}
+)
+
+var (
+	localChannel IChannel
+)
+
+func Channel() IChannel {
+	if localChannel == nil {
+		panic("implement not found for interface IChannel, forgot register?")
+	}
+	return localChannel
+}
+
+func RegisterChannel(i IChannel) {
+	localChannel = i
+}
