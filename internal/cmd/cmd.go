29e62b1e04e16cbf6d9548f26264df964d385027
Wisansiiz <18257719723@163.com>
2024-03-10 23:10:05 +0800

    feat: 路由

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -2,12 +2,21 @@ package cmd
 
 import (
 	"context"
+	"github.com/goflyfox/gtoken/gtoken"
+	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"github.com/gogf/gf/v2/os/gcmd"
+	"github.com/gogf/gf/v2/os/gtime"
+	"github.com/gogf/gf/v2/util/gconv"
+	"voichatter/internal/controller/channel"
 	"voichatter/internal/controller/chat"
-	"voichatter/internal/controller/hello"
+	sgroup "voichatter/internal/controller/group"
+	"voichatter/internal/controller/message"
+	"voichatter/internal/controller/server"
 	"voichatter/internal/controller/user"
+	"voichatter/internal/model"
+	"voichatter/internal/model/entity"
 	"voichatter/internal/service"
 )
 
@@ -51,23 +60,38 @@ var (
 				})
 				group.Middleware(
 					ghttp.MiddlewareCORS, //跨域中间件
-					service.Middleware().Ctx,
 					ghttp.MiddlewareHandlerResponse,
 				)
-				group.Group("/", func(group *ghttp.RouterGroup) {
-					group.POST("/", user.NewV1().SignIn)
-					group.POST("/", user.NewV1().SignUp)
-					//group.GET("/", user.NewV1().ServerList)
+				// 启动gToken
+				gfToken := &gtoken.GfToken{
+					LoginPath:        "/login",
+					CacheMode:        2,
+					LoginBeforeFunc:  service.User().LoginFunc,
+					LogoutPath:       "/logout",
+					AuthAfterFunc:    authFunc,
+					LoginAfterFunc:   loginAfterFunc,
+					AuthExcludePaths: g.SliceStr{"/register, /login"},
+					GlobalMiddleware: true, // 开启全局拦截，默认关闭
+				}
+				group.Group("/api", func(group *ghttp.RouterGroup) {
+					group.GET("/yy", func(r *ghttp.Request) {
+						chat.GroupChat(gfToken, r)
+					})
+					group.GET("/wz", func(r *ghttp.Request) {
+						chat.MessageSend(gfToken, r)
+					})
 				})
-				group.GET("/api/yy", func(r *ghttp.Request) {
-					chat.HandleWebSocket(r)
-				})
 				group.Group("/", func(group *ghttp.RouterGroup) {
-					group.Middleware(service.Middleware().Auth)
+					err := gfToken.Middleware(ctx, group)
+					if err != nil {
+						panic(err)
+					}
 					group.Bind(
-						hello.NewV1(),
-						user.NewV1().SignOut,
-						user.NewV1().ServerList,
+						sgroup.NewV1(),
+						user.NewV1(),
+						server.NewV1(),
+						message.NewV1(),
+						channel.NewV1(),
 					)
 				})
 			})
@@ -78,3 +102,73 @@ var (
 		},
 	}
 )
+
+func authFunc(r *ghttp.Request, respData gtoken.Resp) {
+	var userInfo model.LoginRes
+	respData.Code = gtoken.SUCCESS
+	var s gTokenData
+	err := gconv.Struct(respData.Data, &s)
+	if err != nil {
+		r.SetError(gerror.New("验证失败"))
+		r.Exit()
+	}
+	userInfo = model.LoginRes{
+		UserId:        uint64(s.Data.UserId),
+		Username:      s.Data.Username,
+		Email:         s.Data.Email,
+		AvatarUrl:     s.Data.AvatarUrl,
+		LastLoginDate: &s.Data.LastLoginDate,
+	}
+	r.SetCtxVar("userId", userInfo.UserId)
+	r.SetCtxVar("username", userInfo.Username)
+	r.Middleware.Next()
+}
+
+type gTokenData struct {
+	CreateTime int64 `json:"createTime"`
+	Data       struct {
+		UserId           int         `json:"user_id"`
+		Username         string      `json:"username"`
+		Email            string      `json:"email"`
+		PasswordHash     string      `json:"password_hash"`
+		AvatarUrl        string      `json:"avatar_url"`
+		RegistrationDate gtime.Time  `json:"registration_date"`
+		LastLoginDate    gtime.Time  `json:"last_login_date"`
+		DeletedAt        interface{} `json:"deleted_at"`
+	} `json:"data"`
+	RefreshTime int64  `json:"refreshTime"`
+	UserKey     string `json:"userKey"`
+	Uuid        string `json:"uuid"`
+}
+
+func loginAfterFunc(r *ghttp.Request, respData gtoken.Resp) {
+	type data struct {
+		Uuid    string `json:"uuid"`
+		Token   string `json:"token"`
+		UserKey string `json:"userKey"`
+	}
+	var dataRep data
+	err := gconv.Struct(respData.Data, &dataRep)
+	if err != nil {
+		r.SetError(gerror.New("数据错误"))
+		r.Exit()
+	}
+	userId := gconv.Uint64(dataRep.UserKey)
+	var userInfo model.LoginRes
+	err = g.Model(entity.User{}).Where("user_id = ?", userId).Scan(&userInfo)
+	if err != nil {
+		r.SetError(gerror.New("数据错误"))
+		r.Exit()
+	}
+	r.Response.WriteJson(g.Map{
+		"code":    0,
+		"message": "登录成功",
+		"data": g.Map{
+			"token":     dataRep.Token,
+			"userId":    userInfo.UserId,
+			"username":  userInfo.Username,
+			"avatarUrl": userInfo.AvatarUrl,
+		},
+	})
+	r.Middleware.Next()
+}
