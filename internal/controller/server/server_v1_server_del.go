f7333d3d7758edfd48bda1a162f67e4894d14337
Wisansiiz <18257719723@163.com>
2024-03-10 23:09:57 +0800

    feat: 删除服务器

--- internal/controller/server/server_v1_server_del.go
+++ internal/controller/server/server_v1_server_del.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"context"
-	"github.com/gogf/gf/v2/errors/gerror"
 	"voichatter/internal/service"
 
 	"voichatter/api/server/v1"
@@ -11,7 +10,7 @@ import (
 func (c *ControllerV1) ServerDel(ctx context.Context, req *v1.ServerDelReq) (res *v1.ServerDelRes, err error) {
 	_, err = service.Server().ServerDel(ctx, req.ServerId)
 	if err != nil {
-		return nil, gerror.New("权限不足")
+		return nil, err
 	}
 	return nil, nil
 }
