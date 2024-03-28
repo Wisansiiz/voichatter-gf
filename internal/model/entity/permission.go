c9dc576ef2033b6d66077dbc88f0bf524c7ae6a8
Wisansiiz <18257719723@163.com>
2024-03-28 18:02:55 +0800

    feat: 权限表

---
+++ internal/model/entity/permission.go
@@ -0,0 +1,10 @@
+// =================================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// =================================================================================
+
+package entity
+
+// Permission is the golang structure for table permission.
+type Permission struct {
+	PermissionType string `json:"permission_type" ` //
+}
