307d4c3053c59f4105d01b478858ece6d32e5f1a
Wisansiiz <18257719723@163.com>
2024-03-28 18:52:20 +0800

    feat: 通知

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -1,9 +1,10 @@
 package consts
 
 const (
-	GroupList   = "GROUP_LIST"
-	ChannelList = "CHANNEL_LIST"
-	ServerList  = "SERVER_LIST"
-	ServerId    = "SERVER_ID"
-	ServerUsers = "SERVER_USERS"
+	GroupList    = "GROUP_LIST"
+	ChannelList  = "CHANNEL_LIST"
+	ServerList   = "SERVER_LIST"
+	ServerId     = "SERVER_ID"
+	ServerUsers  = "SERVER_USERS"
+	Notification = "NOTIFICATION"
 )
