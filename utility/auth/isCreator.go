27df1e7dce4a27c427fb9688754b92a20e6e4fe7
Wisansiiz <18257719723@163.com>
2024-03-29 14:56:36 +0800

    feat: 创建者验证

---
+++ utility/auth/isCreator.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"context"
+	"github.com/gogf/gf/v2/util/gconv"
+	"voichatter/internal/dao"
+	"voichatter/utility/errResponse"
+)
+
+func IsServerCreator(ctx context.Context, serverId uint64) (err error) {
+	userId := gconv.Uint64(ctx.Value("userId"))
+	count, err := dao.Server.Ctx(ctx).Where("server_id = ? AND creator_user_id = ?", serverId, userId).Count()
+	if err != nil {
+		return errResponse.DbOperationError("操作失败")
+	}
+	if count == 0 {
+		return errResponse.OperationFailed("权限不足")
+	}
+	return nil
+}
