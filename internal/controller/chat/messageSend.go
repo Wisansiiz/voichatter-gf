af42f653e2e598ad8398e5a1b54b42a108213e2b
Wisansiiz <18257719723@163.com>
2024-03-30 23:32:16 +0800

    refactor: 修改数据的结构

--- internal/controller/chat/messageSend.go
+++ internal/controller/chat/messageSend.go
@@ -8,13 +8,11 @@ import (
 	"github.com/gogf/gf/v2/os/gtime"
 	"github.com/gogf/gf/v2/util/gconv"
 	"github.com/gorilla/websocket"
-	"voichatter/internal/dao"
 	"voichatter/internal/model"
-	"voichatter/internal/model/entity"
 )
 
 var (
-	rooms = make(map[uint64][]*websocket.Conn)
+	rooms = make(map[uint64][]map[uint64]*websocket.Conn)
 )
 
 func MessageSend(r *ghttp.Request) {
@@ -32,49 +30,63 @@ func MessageSend(r *ghttp.Request) {
 	}(ws)
 
 	userId := r.GetCtxVar("userId").Uint64()
-	// 从URL参数获取频道号
-	serverId := r.GetQuery("serverId").Uint64()
-	channelId := r.GetQuery("channelId").Uint64()
-	// 将连接加入到对应的房间
-	rooms[channelId] = append(rooms[channelId], conn)
+	g.Dump(userId)
 	// 接收和处理消息
+	var targetId uint64
 	for {
 		_, p, err := conn.ReadMessage()
 		if err != nil {
 			break
 		}
-		//var msg Msg
+		//content := string(p)
+		str, _ := gjson.DecodeToJson(p)
+		targetId = str.Get("targetId").Uint64()
+		for i, m := range rooms[targetId] {
+			if c, ok := m[userId]; ok {
+				if c != conn {
+					delete(m, userId)
+					m[userId] = conn
+					break
+				} else {
+					break
+				}
+			}
+			if i == len(rooms[targetId])-1 {
+				currentUser := make(map[uint64]*websocket.Conn)
+				currentUser[userId] = conn
+				rooms[targetId] = append(rooms[targetId], currentUser)
+			}
+		}
+		if len(rooms[targetId]) == 0 {
+			currentUser := make(map[uint64]*websocket.Conn)
+			currentUser[userId] = conn
+			rooms[targetId] = append(rooms[targetId], currentUser)
+		}
+		//if str.Get("data").String() != "" {
+		//	content = str.Get("data").String()
+		//}
+		//if gconv.String(str.Map()["code"]) == "leave" {
+		//	clean(targetId, userId)
+		//}
 		// 持久化消息到数据库
-		content := string(p)
-		//err = gjson.Unmarshal(p, &msg)
+		//_, err = dao.Message.Ctx(r.Context()).Insert(
+		//	&entity.Message{
+		//		SenderUserId: userId,
+		//		Content:      content,
+		//		ChannelId:    channelId,
+		//		ServerId:     serverId,
+		//		SendDate:     gtime.Now(),
+		//	},
+		//)
 		//if err != nil {
+		//	r.SetError(gerror.New("持久化出错"))
 		//	return
 		//}
-		str, _ := gjson.DecodeToJson(p)
-
-		//g.Dump(msg)
-		g.Dump(str.Map()["data"])
-		if gconv.String(str.Map()["data"]) != "" {
-			content = gconv.String(str.Map()["data"])
-		}
-		_, err = dao.Message.Ctx(r.Context()).Insert(
-			&entity.Message{
-				SenderUserId: userId,
-				Content:      content,
-				ChannelId:    channelId,
-				ServerId:     serverId,
-				SendDate:     gtime.Now(),
-			},
-		)
-		if err != nil {
-			r.SetError(gerror.New("持久化出错"))
-			return
-		}
 		var messageInfo = model.MessageInfo{
-			MessageId:    gconv.Uint64(gconv.String(str.Map()["messageId"])),
-			ChannelId:    channelId,
-			Content:      content,
-			ServerId:     serverId,
+			MessageId: 0,
+			ChannelId: targetId,
+			Content:   str.Get("data").String(),
+			//ServerId:     serverId,
 			Attachment:   "",
 			AvatarUrl:    r.GetCtxVar("avatarUrl").String(),
 			SenderUserId: userId,
@@ -82,19 +94,20 @@ func MessageSend(r *ghttp.Request) {
 			SendDate:     gtime.Now(),
 		}
 		// 将消息广播给房间内的所有客户端
-		go broadcastMessage(channelId, gconv.Bytes(g.Map{"message": messageInfo}))
+		g.Dump(str.Get("code").String())
+		go broadcastMessage(targetId, gconv.Bytes(g.Map{"count": len(rooms[targetId]), "code": str.Get("code").String(), "message": messageInfo}))
 	}
-
-	defer clean(channelId, conn)
+	defer clean(targetId, userId)
 }
 
-func clean(channelId uint64, conn *websocket.Conn) {
+func clean(channelId uint64, userId uint64) {
 	mu.Lock()
 	defer mu.Unlock()
 	// 在连接关闭时，将其从房间中移除
 	connections := rooms[channelId]
 	for i, number := range connections {
-		if number == conn {
+		if _, ok := number[userId]; ok {
+			delete(number, userId)
 			rooms[channelId] = append(connections[:i], connections[i+1:]...)
 			break
 		}
@@ -108,8 +121,10 @@ func broadcastMessage(channelId uint64, message []byte) {
 	connections := rooms[channelId]
 	for _, conn := range connections {
 		// 发送消息给所有客户端包括自己
-		if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
-			return
+		for _, v := range conn {
+			if err := v.WriteMessage(websocket.TextMessage, message); err != nil {
+				return
+			}
 		}
 	}
 }
