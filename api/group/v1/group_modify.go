c0282edf7c872b75166757130c7efdc143112f16
Wisansiiz <18257719723@163.com>
2024-04-10 15:29:37 +0800

    feat: 分组名称修改

--- api/group/v1/group_modify.go
+++ api/group/v1/group_modify.go
@@ -7,9 +7,9 @@ import (
 
 type GroupModifyReq struct {
 	g.Meta    `path:"/group/{serverId}/{groupId}" method:"put" tags:"GroupService" summary:"修改分组名称"`
-	ServerId  uint64 `p:"serverId" v:"required"`
-	GroupId   uint64 `p:"groupId" v:"required"`
-	GroupName string `p:"groupName" v:"required|length:4,10#请输入分组名|分组名长度为:{min}到:{max}位"`
+	ServerId  uint64 `p:"serverId"  v:"required"`
+	GroupId   uint64 `p:"groupId"   v:"required"`
+	GroupName string `p:"groupName" v:"required|length:1,10#请输入分组名|分组名长度为:{min}到:{max}位"`
 }
 
 type GroupModifyRes struct {
